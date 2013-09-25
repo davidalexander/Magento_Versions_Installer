02ab39c3fc5c056e8be7de22ed69b27e9e442afe
David Alexander <david@skywire.co.uk>
2013-09-25 13:31:24 +0100

    add v1.8 to the versions array

--- magento_versions_installer.sh
+++ magento_versions_installer.sh
@@ -2,7 +2,7 @@
 
 SITES_DIR="/Users/david/Sites/"
 SAMPLE_DATA_VERSION="1.6.1.0"
-MAGENTO_VERSIONS_ARRAY=("1.4.0.1" "1.4.1.0" "1.4.1.1" "1.4.2.0" "1.5.0.1" "1.5.1.0" "1.6.0.0" "1.6.1.0" "1.6.2.0" "1.7.0.0" "1.7.0.2")
+MAGENTO_VERSIONS_ARRAY=("1.4.0.1" "1.4.1.0" "1.4.1.1" "1.4.2.0" "1.5.0.1" "1.5.1.0" "1.6.0.0" "1.6.1.0" "1.6.2.0" "1.7.0.0" "1.7.0.2" "1.8.0.0")
 MAGENTO_SAFE_VERSIONS_ARRAY=( "${MAGENTO_VERSIONS_ARRAY[@]//./}" )
 PHP_PATH="/Applications/MAMP/bin/php/php5.3.6/bin/php"
 SQL_PATH="/Applications/MAMP/Library/bin/mysql"
