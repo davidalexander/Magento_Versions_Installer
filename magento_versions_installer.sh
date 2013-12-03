509973f535beeafa4438b8c5a048102fba5a77a0
David Alexander <david@skywire.co.uk>
2013-12-03 00:02:00 +0000

    update install directory

--- magento_versions_installer.sh
+++ magento_versions_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SITES_DIR="/Users/david/Sites/"
+SITES_DIR="/Users/david/Sites/magento/"
 SAMPLE_DATA_VERSION="1.6.1.0"
 MAGENTO_VERSIONS_ARRAY=("1.4.0.1" "1.4.1.0" "1.4.1.1" "1.4.2.0" "1.5.0.1" "1.5.1.0" "1.6.0.0" "1.6.1.0" "1.6.2.0" "1.7.0.0" "1.7.0.2" "1.8.0.0")
 MAGENTO_SAFE_VERSIONS_ARRAY=( "${MAGENTO_VERSIONS_ARRAY[@]//./}" )
